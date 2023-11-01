e0f91311d5fbeb51bcb4bff15c32acd1c9391f4c
Ayesha <pu_ash@yahoo.com>
2023-11-01 11:49:27 +0000

    Added functionality to render the variable to result tag.

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ function addition(event) {
   const num2 = document.getElementById("num2").value;
 
   let sum = parseInt(num1) + parseInt(num2);
+  let result = `${num1} + ${num2} = ${sum}`;
   document.getElementById("form").reset();
 
   // displaying result
-  document.getElementById("result").innerHTML = sum;
+  document.getElementById("result").innerHTML = result;
 }
 form.addEventListener("submit", addition);
